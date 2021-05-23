d1c1c7a86b7968ccdc8d5d13328abf2d86c21eef
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2021-05-23 09:19:49 +0200

    Update: main code+ branch ID added + Backup feature added

---
+++ cmake-build-debug/CMakeFiles/CS316LastTask.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS316LastTask.dir/main.cpp.obj"
+  "CS316LastTask.exe"
+  "CS316LastTask.exe.manifest"
+  "CS316LastTask.pdb"
+  "libCS316LastTask.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS316LastTask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
